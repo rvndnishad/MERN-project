0bc73a0c0d866fa087bcd125e20ab7f5fe369f31
Arvind Nishad <rvndnishad@gmail.com>
2018-09-17 15:53:11 +0530

    removed Jarallax

--- client/src/containers/DefaultLayout/DefaultHeader.js
+++ client/src/containers/DefaultLayout/DefaultHeader.js
@@ -6,7 +6,7 @@ import { BrowserRouter, Link } from "react-router-dom";
 import PropTypes from 'prop-types';
 import { logoutUser } from './../../actions/authActions';
 import { AppAsideToggler, AppHeaderDropdown, AppNavbarBrand, AppSidebarToggler } from '@coreui/react';
-import logo from '';
+import logo from '../../asset/CinemaInABox.png';
 import sygnet from '../../asset/speedometer.svg';
 
 import { connect } from 'react-redux';
@@ -36,7 +36,6 @@ class DefaultHeader extends Component {
       <React.Fragment>
         <AppSidebarToggler className="d-lg-none" display="md" mobile />
         <AppNavbarBrand
-          full={{ src: logo, width: 30, height: 40, alt: 'Cinema In A Box' }}
           minimized={{ src: sygnet, width: 30, height: 40, alt: 'Cinema In A Box' }}
         />
         <AppSidebarToggler className="d-md-down-none left-toggler" display="lg" />
